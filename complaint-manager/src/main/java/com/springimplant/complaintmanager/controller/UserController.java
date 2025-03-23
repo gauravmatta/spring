79adaafbee8ddef71d63b9abc48b38bc41b5db40
Gaurav Matta <gaurav.matta.tech@gmail.com>
2025-03-23 16:05:00 +0530

    Added PUT and GET and DELETE methods

--- complaint-manager/src/main/java/com/springimplant/complaintmanager/controller/UserController.java
+++ complaint-manager/src/main/java/com/springimplant/complaintmanager/controller/UserController.java
@@ -7,9 +7,12 @@ import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -63,7 +66,7 @@ public class UserController {
 	return "getUser";
 	}
 	
-	@GetMapping("/list")
+	@GetMapping(path="/list")
 	@ResponseBody
 	public List<Users> getUsers() {
 		return usersRepository.findAll();
@@ -74,5 +77,30 @@ public class UserController {
 	public Optional<Users> getUser(@PathVariable("id") int id) {
 		return usersRepository.findById(id);
 	}
-
+	
+	@PostMapping(path="/adduser",consumes = {"application/json"})
+	@ResponseBody
+	public Users addRestUser(@RequestBody Users user) {
+		System.out.println(user);
+		usersRepository.save(user);
+		return user;
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	@ResponseBody
+	public Users deleteUser(@PathVariable int id) {
+	Users user =usersRepository.getReferenceById(id);
+	usersRepository.delete(user);
+	return user;
+	}
+	
+	@PutMapping(path="/user",consumes = {"application/json"})
+	@ResponseBody
+	public Users updateRestUser(@RequestBody Users user) {
+		System.out.println(user);
+		usersRepository.save(user);
+		return user;
+	}
+	
+	
 }
