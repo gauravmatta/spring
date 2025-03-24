ab83e97bac703bcf50146e17d07a195e877082de
Gaurav Matta <gaurav.matta.tech@gmail.com>
2025-03-24 11:50:18 +0530

    added Exception Test

---
+++ complaint-manager/src/test/java/com/springimplant/complaintmanager/service/ExceptionTest.java
@@ -0,0 +1,15 @@
+package com.springimplant.complaintmanager.service;
+
+import com.springimplant.complaintmanager.exception.UserNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ExceptionTest {
+
+    @Test
+    public void checkGlobalExceptionHandler(){
+
+    }
+}
